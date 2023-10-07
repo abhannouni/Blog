49798fcf586cdcc3acea51b558b5c555ca626285
abhannouni <abdeslam.hannouni@gmail.com>
2023-10-07 18:32:01 +0100

    Installation of dependencies and establishing a connection with the database

--- config/db.js
+++ config/db.js
@@ -1,6 +1,16 @@
-module.exports = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'blog',
-  };
+const mysql = require('mysql2');
+
+// Create a connection pool
+const dbPool = mysql.createPool({
+  host: "127.0.0.1",
+  user: "root",
+  port: 3306,
+  password: "",
+  database: "blog",
+  waitForConnections: true, // Set to true to enable pooling
+  connectionLimit: 10, // Adjust the limit based on your needs
+  queueLimit: 0, // Unlimited queueing
+});
+
+module.exports = dbPool;
+
